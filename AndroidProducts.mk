430ee7c0a56d9a839f41edb0197be5e4e1626460
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-05-27 19:16:22 +0200

    rk3328_box: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: rockchip Model: H96_Max_V12 Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_rk3328_box.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_rk3328_box-user \
+    omni_rk3328_box-userdebug \
+    omni_rk3328_box-eng
---
+++ omni_rk3328_box.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from rk3328_box device
+$(call inherit-product, device/rockchip/rk3328_box/device.mk)
+
+PRODUCT_DEVICE := rk3328_box
+PRODUCT_NAME := omni_rk3328_box
+PRODUCT_BRAND := Rockchip
+PRODUCT_MODEL := H96_Max_V12
+PRODUCT_MANUFACTURER := rockchip
+
+PRODUCT_GMS_CLIENTID_BASE := android-rockchip-tv
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="rk3328_box-userdebug 12 SQ3A.220605.009.B1 eng.pc.20221117.145212 release-keys"
+
+BUILD_FINGERPRINT := google/redfin/redfin:12/SP1A.210812.015/7679548:user/release-keys__2021-10-05
